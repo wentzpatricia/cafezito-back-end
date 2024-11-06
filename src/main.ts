75ca24bfbf285b163965f1b73c0af0d817081e5d
Mariana Dreyer <mariana.odreyer@gmail.com>
2024-11-06 15:23:12 -0300

    Deploy back-end parcial 6

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+const port = process.env.PORT || 3000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
@@ -13,6 +13,11 @@ async function bootstrap() {
 
   app.enableCors();
   
-  await app.listen(3000);
+  await app.listen(port);
+
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
+
+
+
